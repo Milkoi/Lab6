0afb70dc45680eea07c9dd4b65590ba43e3c708e
Milkoi <minhlequan2005@gmail.com>
2026-02-04 21:58:56 +0700

    Add Lab6 Laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Trang liên hệ</h1>
+    <p>Email: minhlequan2005@gmail.com</p>
+    <p>Phone: 0327078744</p>
+</body>
+</html>
+
+
